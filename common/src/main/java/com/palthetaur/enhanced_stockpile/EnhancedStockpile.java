5f53c48fd13f450a4ee9a105b612822ee0778707
Pal <dantdm0126@gmail.com>
2024-07-12 10:31:23 -0500

    work? (clueless on what im doing)

---
+++ common/src/main/java/com/palthetaur/enhanced_stockpile/EnhancedStockpile.java
@@ -0,0 +1,6 @@
+package com.palthetaur.enhanced_stockpile;
+
+
+public class EnhancedStockpile {
+    public static final String MOD_ID = "enhanced_stockpile";
+}
---
+++ fabric/src/main/java/com/palthetaur/enhanced_stockpile/fabric/EnhancedStockpileFabric.java
@@ -0,0 +1,9 @@
+package com.palthetaur.enhanced_stockpile.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EnhancedStockpileFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
